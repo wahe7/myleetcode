3103c885e80960d6dbeca5d6841fdc7b743629b0
waheguru singh <105860842+wahe7@users.noreply.github.com>
2024-06-11 16:41:50 +0530

    Time: 12 ms (97.01%), Space: 46.6 MB (36.23%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n=nums.length;
+        
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0){
+                sum+=nums[i];
+            }
+            
+        }
+        
+        return sum;
+    }
+}
